069c6d712b94da6a53890244604353c4d794d4f5
Alberto Muñoz <albertososamunoz@gmail.com>
2022-03-27 12:01:48 +0200

    añadidos spatie webhook server y client

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\WebhookServer\WebhookCall;
+use Illuminate\Support\Facades\Log; 
+
 
 class WebhookController extends Controller
 {
-    //
+    
+    public function send_webhook(){
+
+        WebhookCall::create()
+            ->url('127.0.0.1:8000/api/webhook-radar')
+            ->payload(['action' => 'creado y enciado'])
+            ->useSecret('sin secretos')
+            ->dispatch();
+
+        Log::info("creado");
+    }
+
 }
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        '/webhook-radar',
+        'webhook-radar'
     ];
 }
